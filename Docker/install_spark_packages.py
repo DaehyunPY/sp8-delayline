270eb4026680ece0debdae787cfffb7b1bb3ee32
Daehyun You <daehyun.park.you@gmail.com>
2018-08-25 15:24:54 +0900

    Add a todo comment (+1 squashed commit) Squashed commits: [48df186] Install KryoSerializer (but it is not used in PySpark code)

--- Docker/install_spark_packages.py
+++ Docker/install_spark_packages.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 builder = (SparkSession
            .builder
            .config("spark.jars.packages", "org.diana-hep:spark-root_2.11:0.1.15")
+           .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
            )
 with builder.getOrCreate():
     pass
